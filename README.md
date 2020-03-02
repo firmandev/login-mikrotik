5b370467f1ebbbbd5d3202f026af5d7bf0506f6d
firman <firman@hellobill.co.id>
2020-03-02 23:39:11 +0700

    fix some bugs after tested and update README.md

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # login-mikrotik
 Hello my name is firman, this is my first open source for login to mikrotik.
 Because i'm using wifi id as my primary internet it is annoyed when every 24 hours i got kicked, so this script is only support wifi id 
-only, but i promise in the future i will support many mikrotik. 
+only, but i promise in the future it will support many mikrotik. 
 
-This is how to use my script for login :
--- i will write it later --
+# This is how to use my script for login
+1. You need to install npm and node js.
+https://nodejs.org/en/download/
+https://www.npmjs.com/get-npm
 
+2. Install puppeteer 
+npm i puppeteer
+
+3. You need to change the constant.js
+if you are using wifi id, here is the setting.
+module.exports = {
+    username            : 'your pass',
+    password            : 'your password',
+    userElement         : '#username_member',
+    userElementPassword : '#password_member',
+    url                 : 'https://welcome2.wifi.id/login/',
+    loginElementButton  : '.button-lg',
+    loginAfterElement   : '.skinner-right'
+}
 
 
--- scrapping.js
+++ scrapping.js
@@ -4,32 +4,22 @@
 
 const PUPPETEER   = require('puppeteer');
 const C           = require('./constants');
-const USERID      = '#login-username';
-const PASSWORD    = '#login-password';
-const URL         = 'http://welcome2.wifi.id/login/?gw_id=WAG-D2-JT&client_mac=40:9f:38:ea:f4:35&wlan=GPON02-D2-CPD-3%20pon%201/1/15/4/25/5:135&sessionid=WAG-D2-04100140300000a2bb58229504';
-const LOGINBUTTON = '';
 
-async function login(params){
+async function login(){
   let launchOptions = { headless: false };
   const browser = await PUPPETEER.launch(launchOptions);
   const page = await browser.newPage();
   await page.setViewport({width: 1366, height: 768});
-  await page.goto(params.URL);
-  await page.waitForSelector(params.userElement);
-  await page.type(params.userElement, C.username);
-  await page.waitForSelector(params.passElement);
-  await page.type(params.passElement, C.password);
-  await page.click(LOGINBUTTON);
+  await page.goto(C.url);
+  await page.waitForSelector(C.userElement);
+  await page.type(C.userElement, C.username);
+  await page.waitForSelector(C.userElementPassword);
+  await page.type(C.userElementPassword, C.password);
+  await page.click(C.loginElementButton);
+  await page.waitForSelector(C.loginAfterElement);
 }
 
 (async () => {
-  let params = [];
-  params.push({
-     userElement  : USERID,
-     passElement  : PASSWORD,
-     loginElement : LOGINBUTTON,
-     url          : URL
-  });
-  await login(params);
+  await login();
   process.exit(1);
 })();
